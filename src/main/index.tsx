7bf86e4f6c128e54e246ac61691588c80658275e
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-03-03 14:01:00 -0300

    feat: create AppNavigator to be used in the project

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
 import { registerRootComponent } from 'expo';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppNavigator } from '~/presentation/navigation';
 
 export default function Main() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigator />;
 }
 
 registerRootComponent(Main);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/presentation/navigation/app-navigator.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { routes } from '~/presentation/navigation/routes';
+
+// Screens
+import { CartScreen, HomeScreen } from '~/presentation/screens';
+
+// Components
+import { NavigationCartIcon } from '~/presentation/components';
+
+// Types
+import { RootStackParamList } from './types';
+
+export function AppNavigator() {
+  const Stack = createNativeStackNavigator<RootStackParamList>();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerRight: () => <NavigationCartIcon /> }}>
+        <Stack.Screen
+          name={routes.HomeScreen}
+          component={HomeScreen}
+          options={{ title: 'Produtos' }}
+        />
+        <Stack.Screen
+          name={routes.CartScreen}
+          component={CartScreen}
+          options={{ title: 'Carrinho' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
