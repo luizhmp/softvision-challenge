45c84c61c5d751726c527b2de952006b97557cec
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-03-03 14:02:45 -0300

    feat: create basic structure for CartScreen

---
+++ src/presentation/screens/cart-screen/presentational.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function CartScreen(): JSX.Element {
+  return (
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+      <Text>This is the Cart Screen</Text>
+    </View>
+  );
+}
