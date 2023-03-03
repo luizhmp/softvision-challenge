f74d731f9f4840a225b784e73fcfaf0848f5ec30
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-03-03 14:03:06 -0300

    feat: create basic structure for HomeScreen

---
+++ src/presentation/screens/home-screen/presentational.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function HomeScreen(): JSX.Element {
+  return (
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+      <Text>This is the Home Screen</Text>
+    </View>
+  );
+}
