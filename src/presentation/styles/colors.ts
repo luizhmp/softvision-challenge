b1e61f7e76eb7479b6c16a857153dd6810d0e802
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-03-05 16:01:46 -0300

    feat: add textColor to project

--- src/presentation/styles/colors.ts
+++ src/presentation/styles/colors.ts
@@ -1,9 +1,11 @@
 export interface ColorsInterface {
   black: string;
+  textColor: string;
   white: string;
 }
 
 export const colors = {
   black: '#000000',
+  textColor: '#333333',
   white: '#FFFFFF',
 };
