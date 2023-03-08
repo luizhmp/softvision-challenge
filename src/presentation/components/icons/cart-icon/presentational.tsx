dc83ab4a1d81cc5649550102103fe6c5946d48aa
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-03-08 17:10:54 -0300

    feat: create quantity indicator for cart icon

---
+++ src/presentation/components/icons/cart-icon/presentational.tsx
@@ -0,0 +1,69 @@
+import React, { useContext, useState, useCallback, useEffect } from 'react';
+import { ThemeContext } from 'styled-components';
+import { useNavigation } from '@react-navigation/native';
+import { routes } from '~/presentation/navigation/routes';
+
+// Icons
+import { AntDesign } from '@expo/vector-icons';
+
+// Redux
+import { useSelector } from 'react-redux';
+
+// Styles
+import { UserCheckoutProductsTotalQuantityContainer, WhiteHeading } from './styles';
+
+// Types
+import { StackNavigation } from '~/presentation/navigation/navigators/types';
+import { RootState } from '~/presentation/redux/store/redux-store';
+import { UserCheckoutProducts } from '~/presentation/redux/slices/user-checkout-products/types';
+
+export function CartIcon() {
+  const [totalProductsInCart, setTotalProductsInCart] = useState<number>(0);
+  const navigation = useNavigation<StackNavigation>();
+  const userCheckoutProducts: UserCheckoutProducts[] = useSelector(
+    (state: RootState) => state.userCheckoutProducts.products,
+  );
+
+  const calculateTotalOfProductsInCart = useCallback(() => {
+    const totalProductsInCart = userCheckoutProducts.reduce((acc, currentProduct) => {
+      return acc + currentProduct.userProductQuantity;
+    }, 0);
+    setTotalProductsInCart(totalProductsInCart);
+  }, [userCheckoutProducts]);
+
+  useEffect(() => {
+    calculateTotalOfProductsInCart();
+  }, [calculateTotalOfProductsInCart]);
+
+  function goToCartScreen() {
+    navigation.navigate(routes.CartScreen);
+  }
+
+  const { colors } = useContext(ThemeContext);
+
+  function renderQuantityIndicator() {
+    const hasZeroProducts = totalProductsInCart === 0;
+
+    if (hasZeroProducts) {
+      return null;
+    }
+
+    return (
+      <UserCheckoutProductsTotalQuantityContainer>
+        <WhiteHeading type="H1">{totalProductsInCart}</WhiteHeading>
+      </UserCheckoutProductsTotalQuantityContainer>
+    );
+  }
+
+  return (
+    <AntDesign.Button
+      name="shoppingcart"
+      size={24}
+      color={colors.black}
+      backgroundColor={colors.white}
+      onPress={goToCartScreen}
+    >
+      {renderQuantityIndicator()}
+    </AntDesign.Button>
+  );
+}
---
+++ src/presentation/components/icons/cart-icon/styles.ts
@@ -0,0 +1,29 @@
+import styled, { css } from 'styled-components/native';
+
+// Components
+import { Heading } from '~/presentation/components/texts';
+
+export const UserCheckoutProductsTotalQuantityContainer = styled.View`
+  ${({ theme }) => {
+    return css`
+      z-index: 999;
+      width: 24px;
+      height: 24px;
+      position: absolute;
+      right: 0;
+      top: 0;
+      border-radius: ${theme.metrics.circle}px;
+      justify-content: center;
+      align-items: center;
+      background-color: ${theme.colors.secondary};
+    `;
+  }};
+`;
+
+export const WhiteHeading = styled(Heading)`
+  ${({ theme }) => {
+    return css`
+      color: ${theme.colors.white};
+    `;
+  }};
+`;
