820fad9be76a4a48559636f4dc51091ec6fdf5f8
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-03-08 17:36:46 -0300

    feat: use NavigationHeaderTitle component and change background-color

--- src/presentation/navigation/navigators/app-navigator.tsx
+++ src/presentation/navigation/navigators/app-navigator.tsx
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { routes } from '~/presentation/navigation/routes';
+import { ThemeContext } from 'styled-components';
+
+// Components
+import { CartIcon } from '~/presentation/components/icons';
+import { NavigationHeaderTitle } from '~/presentation/components';
 
 // Screens
 import { CartScreen, HomeScreen } from '~/presentation/screens';
-
-// Components
-import { CartIcon } from '~/presentation/components';
 
 // Types
 import { RootStackParamList } from './types';
 
 export function AppNavigator() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
+  const { colors } = useContext(ThemeContext);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerRight: () => <CartIcon /> }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.primary,
+          },
+          headerTitleAlign: 'center',
+          headerRight: () => <CartIcon />,
+        }}
+      >
         <Stack.Screen
           name={routes.HomeScreen}
           component={HomeScreen}
-          options={{ title: 'Produtos' }}
+          options={{ headerTitle: () => <NavigationHeaderTitle title="Produtos" /> }}
         />
         <Stack.Screen
           name={routes.CartScreen}
           component={CartScreen}
-          options={{ title: 'Carrinho' }}
+          options={{ headerTitle: () => <NavigationHeaderTitle title="Carrinho" /> }}
         />
       </Stack.Navigator>
     </NavigationContainer>
