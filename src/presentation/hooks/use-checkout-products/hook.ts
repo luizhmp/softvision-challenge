63693973ddbc2b73ce82a022d55dff5653b781d5
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-03-09 00:41:43 -0300

    feat: create useCheckoutProducts hook

---
+++ src/presentation/hooks/use-checkout-products/hook.ts
@@ -0,0 +1,42 @@
+import { useState, useEffect, useCallback } from 'react';
+
+// Redux
+import { useSelector } from 'react-redux';
+
+// Types
+import { RootState } from '~/presentation/redux/store/redux-store';
+import { UserCheckoutProducts } from '~/presentation/redux/slices/user-checkout-products/types';
+
+export function useCheckoutProducts() {
+  const userCheckoutProducts: UserCheckoutProducts[] = useSelector(
+    (state: RootState) => state.userCheckoutProducts.products,
+  );
+  const [totalProductsInCart, setTotalProductsInCart] = useState<number>(0);
+  const [totalValueInCart, setTotalValueInCart] = useState<number>(0);
+
+  const calculateTotalOfProductsInCart = useCallback(() => {
+    const totalProductsInCart = userCheckoutProducts.reduce((acc, currentProduct) => {
+      return acc + currentProduct.userProductQuantity;
+    }, 0);
+
+    setTotalProductsInCart(totalProductsInCart);
+  }, [userCheckoutProducts]);
+
+  useEffect(() => {
+    calculateTotalOfProductsInCart();
+  }, [calculateTotalOfProductsInCart]);
+
+  const calculateTotalValueInCart = useCallback(() => {
+    const totalValueInCart = userCheckoutProducts.reduce((acc, currentProduct) => {
+      return acc + currentProduct.price;
+    }, 0);
+
+    setTotalValueInCart(totalValueInCart);
+  }, [userCheckoutProducts]);
+
+  useEffect(() => {
+    calculateTotalValueInCart();
+  }, [calculateTotalValueInCart]);
+
+  return { totalProductsInCart, totalValueInCart, userCheckoutProducts };
+}
