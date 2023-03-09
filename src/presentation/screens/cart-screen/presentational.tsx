0ab9c2d558030385011f87ba84a204b790dcdb69
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-03-09 01:05:16 -0300

    style: create subtotal

--- src/presentation/screens/cart-screen/presentational.tsx
+++ src/presentation/screens/cart-screen/presentational.tsx
@@ -5,18 +5,21 @@ import { FlatList } from 'react-native';
 import { CheckoutProductCard, EmptyShoppingCart, ListFooter } from '~/presentation/components';
 import { Heading } from '~/presentation/components/texts';
 
+// Helpers
+import { formatNumberIntoCurrency } from '~/presentation/helpers';
+
 // Hooks
 import { useCheckoutProducts } from '~/presentation/hooks';
 
 // Styles
-import { CheckoutProductCardContainer } from './styles';
+import { CheckoutProductCardContainer, SubtotalContainer, WhiteHeading } from './styles';
 import { ThemeContext } from 'styled-components';
 
 // Types
 import { UserCheckoutProducts } from '~/presentation/redux/slices/user-checkout-products/types';
 
 export function CartScreen() {
-  const { totalProductsInCart, userCheckoutProducts } = useCheckoutProducts();
+  const { totalProductsInCart, totalValueInCart, userCheckoutProducts } = useCheckoutProducts();
   const { metrics } = useContext(ThemeContext);
 
   function renderItem(product: UserCheckoutProducts) {
@@ -45,20 +48,40 @@ export function CartScreen() {
     return <EmptyShoppingCart />;
   }
 
+  function renderSubtotal() {
+    const hasProducts = totalProductsInCart > 0;
+
+    if (!hasProducts) {
+      return null;
+    }
+
+    const formattedTotalValueInCart = formatNumberIntoCurrency(totalValueInCart);
+
+    return (
+      <SubtotalContainer>
+        <WhiteHeading type="H1">Subtotal</WhiteHeading>
+        <WhiteHeading type="H2">{formattedTotalValueInCart}</WhiteHeading>
+      </SubtotalContainer>
+    );
+  }
+
   return (
-    <FlatList
-      keyExtractor={(item) => item.id.toString()}
-      data={userCheckoutProducts}
-      contentContainerStyle={{
-        paddingTop: metrics.headerSpace,
-        paddingHorizontal: metrics.screenHorizontalPadding,
-        paddingVertical: metrics.space,
-      }}
-      renderItem={({ item }) => renderItem(item)}
-      showsVerticalScrollIndicator={false}
-      ListHeaderComponent={renderHeader}
-      ListFooterComponent={renderFooter}
-      ListEmptyComponent={renderEmptyComponent}
-    />
+    <>
+      <FlatList
+        keyExtractor={(item) => item.id.toString()}
+        data={userCheckoutProducts}
+        contentContainerStyle={{
+          paddingTop: metrics.headerSpace,
+          paddingHorizontal: metrics.screenHorizontalPadding,
+          paddingVertical: metrics.space,
+        }}
+        renderItem={({ item }) => renderItem(item)}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={renderHeader}
+        ListFooterComponent={renderFooter}
+        ListEmptyComponent={renderEmptyComponent}
+      />
+      {renderSubtotal()}
+    </>
   );
 }
--- src/presentation/screens/cart-screen/styles.ts
+++ src/presentation/screens/cart-screen/styles.ts
@@ -1,9 +1,37 @@
 import styled, { css } from 'styled-components/native';
 
+// Components
+import { Heading } from '~/presentation/components/texts';
+
 export const CheckoutProductCardContainer = styled.View`
   ${({ theme }) => {
     return css`
       margin-top: ${theme.metrics.doubleSpace}px;
+    `;
+  }};
+`;
+
+export const SubtotalContainer = styled.View`
+  ${({ theme }) => {
+    return css`
+      border-top-radius: ${theme.metrics.borderRadius}px;
+      border-top-width: ${theme.metrics.borderWidth}px;
+      border-top-color: ${theme.colors.gray};
+      padding-horizontal: ${theme.metrics.screenHorizontalPadding}px;
+      padding-vertical: ${theme.metrics.space}px;
+      background-color: ${theme.colors.primary}
+      align-items: center;;
+      flex-direction: row;
+      justify-content: space-between;
+    `;
+  }};
+`;
+
+export const WhiteHeading = styled(Heading)`
+  ${({ theme }) => {
+    return css`
+      color: ${theme.colors.white};
+      font-weight: bold;
     `;
   }};
 `;
